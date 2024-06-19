38d2832754f79766343d3216a4858290db3e3e9e
DarkInventor <ktmehta25@gmail.com>
2024-06-18 22:36:16 -0400

    add --logo --metadata

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ import { SiteFooter } from "@/components/site-footer"
 
 export default function IndexPage() {
   const avatarUrls = [
-    "https://avatars.githubusercontent.com/u/16860528",
+    // "https://avatars.githubusercontent.com/u/16860528",
     "https://avatars.githubusercontent.com/u/20110627",
     "https://avatars.githubusercontent.com/u/106103625",
     "https://avatars.githubusercontent.com/u/59228569",
