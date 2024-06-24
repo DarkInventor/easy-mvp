309f09cd918f7b20b65673746b48f691d70da953
DarkInventor <ktmehta25@gmail.com>
2024-06-24 01:26:41 -0400

    adjust orbitting-circles

--- app/integrations/page.tsx
+++ app/integrations/page.tsx
@@ -3,9 +3,9 @@ import OrbitingCircles from "@/components/magicui/orbiting-circles";
 export default function OrbitingCirclesDemo() {
   return (
     <div className="relative flex h-[500px] w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-      {/* <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-6xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center md:text-6xl text-4xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         Integrations
-      </span> */}]
+      </span>
       
 
       {/* Inner Circles */}
